ab433cf5135401c9ef68df9d5aa513b5b4844720
djumanov <diyorbekjumanov1120@gmail.com>
2023-07-08 17:38:57 +0500

    crud task with auth

--- api/views.py
+++ api/views.py
@@ -29,3 +29,39 @@ class TaskList(APIView):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class TaskDetail(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self, request, pk: int) -> Task:
+        try:
+            task = Task.objects.filter(user=request.user).get(id=pk)
+            return task
+        except Task.DoesNotExist:
+            return None
+
+    def get(self, request: Request, pk: int) -> Response:
+        task = self.get_object(request, pk)
+        if task is None:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = TaskSerializer(task)
+        return Response(serializer.data)
+
+    def put(self, request: Request, pk: int) -> Response:
+        task = self.get_object(request, pk)
+        if task is None:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        request.data['user'] = request.user.id
+        serializer = TaskSerializer(task, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request: Request, pk: int) -> Response:
+        task = self.get_object(request, pk)
+        if task is None:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        task.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
