e6973f0386f970fe190c2327ca9c3b1154125ba2
djumanov <diyorbekjumanov1120@gmail.com>
2023-07-08 17:15:35 +0500

    category model

--- api/models.py
+++ api/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+class Category(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='categories')
+    name = models.CharField(max_length=60)
+
+    def __str__(self) -> str:
+        return self.name
  
 class Task(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='tasks')
     title = models.CharField(max_length=60)
     description = models.TextField(blank=True, default='')
     completed = models.BooleanField(default=False)
